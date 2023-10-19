95e36b8f371239f00dbef9e4621d76b1a73a57d0
ciblpaladin <camilotedi@gmail.com>
2023-10-19 00:05:25 -0500

    asdasd

--- Fruver_api/Services/Map_client_data.py
+++ Fruver_api/Services/Map_client_data.py
@@ -4,6 +4,7 @@ class Map_data:
     def data_to_sp(data_client):
 
         data = []
+    
         for key ,values in data_client.data.items():
             data.append(values)
 
@@ -12,16 +13,27 @@ class Map_data:
     def data_build_sp(sp_name ,data_client):
 
         params_build = ""
-        
-        for key, values in data_client.data.items():
+        if isinstance(data_client, dict):
+            for key, values in data_client.data.items():
 
-            params_build += f"{values},"
-            
-        sp_build = f"{sp_name}({params_build})"
+                params_build += f"{values},"
+                
+            sp_build = f"{sp_name}({params_build})"
 
-        if sp_build.endswith(",)"):
-            sp_building = sp_build[:-2]
-            sp_building += ")"
+            if sp_build.endswith(",)"):
+                sp_building = sp_build[:-2]
+                sp_building += ")"
+        else:
+            for values in data_client:
+
+                params_build += f"{values},"
+                
+            sp_build = f"{sp_name}({params_build})"
+
+            if sp_build.endswith(",)"):
+                sp_building = sp_build[:-2]
+                sp_building += ")"
+
 
             return sp_building
      
